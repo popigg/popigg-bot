cc8e5abcf74c505fe6d58f79153e5a524bafc48f
popigg <popigg@gmail.com>
2017-12-25 18:05:30 +0100

    update bot configuration

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 'use strict';
 
 const builder = require('botbuilder');
+const restify = require('restify');
 
-const connector = new builder.ConsoleConnector().listen();
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+   console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+const connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID || '36157f91-00fa-4358-8d5f-bb2a693e7057',
+    appPassword: process.env.MICROSOFT_APP_PASSWORD || 'B@YN/pkOi)I-imzj'
+});
+
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 const bot = new builder.UniversalBot(connector, (session) => {
-    session.send(`You just said: ${session.message.text}`);
+    session.send('You said: %s', session.message.text);
 });
